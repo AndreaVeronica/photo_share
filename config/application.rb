16c932905f17b5c8e791c11c427515827b53349e
Andrea Airall <andreaairall@gmail.com>
2016-04-25 10:03:28 -0400

    Configured custom templates for Scaffolding

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,11 @@ Bundler.require(*Rails.groups)
 
 module PhotoShare
   class Application < Rails::Application
+
+     config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :erb
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
