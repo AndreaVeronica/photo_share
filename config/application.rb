08b773d2f8632196e412de4b41e02ef9e6d799d1
Andrea Airall <andreaairall@gmail.com>
2016-04-28 11:58:59 -0400

    changed css file

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ Bundler.require(*Rails.groups)
 module PhotoShare
   class Application < Rails::Application
 
-      config.serve_static_assets = true
+     #config.serve_static_assets = true
 
      config.generators do |g|
       g.orm             :active_record
