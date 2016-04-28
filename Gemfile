cfe40c99568faa89f404accdc474f2ba5867e143
Andrea Airall <andreaairall@gmail.com>
2016-04-28 12:52:28 -0400

    added AWS access

--- Gemfile
+++ Gemfile
@@ -37,6 +37,13 @@ gem 'bcrypt', '~> 3.1.7'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+  #amazon web services
+  gem 'aws-sdk', '< 2.0'
+
+
+
+
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # sets paperclip to upload images to Amazon 3
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_region => ENV['AWS_REGION'],
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
 end
