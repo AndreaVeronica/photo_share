20dcc9e211d882cc3243dbc0b64ec7c4b1acc662
Andrea Airall <andreaairall@gmail.com>
2016-04-28 20:17:26 -0400

    added styling

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -15,7 +15,11 @@ class Photo < ActiveRecord::Base
   # end
 
 
-has_attached_file :image, styles: {large: "1000x1000>", medium: "600x600>", thumb: "150x150>"}
+has_attached_file :image, styles: {
+  large: "1000x1000>",
+  medium: "300x300>",
+  thumb: "150x150#",
+}
 
 validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
